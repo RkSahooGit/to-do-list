a303630a5b13d098ae0917c5bf44f56ee50f409a
Rakesh Kumar Sahoo <152980437+RkSahooGit@users.noreply.github.com>
2024-03-31 11:20:29 +0530

    Add files via upload

---
+++ To-Do-List/ToDo-List-main/ToDo-List-main/ToDoList-main/ToDoListDev/ToDoListDev/NsObject/ToDoListItems+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItems+CoreDataClass.swift
+//  ToDoListDev
+//
+//  Created by Rakesh Kumar Sahoo on 04/12/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItems)
+public class ToDoListItems: NSManagedObject {
+
+}
